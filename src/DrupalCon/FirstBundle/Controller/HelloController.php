5a0f8bbbf229a00df47b94ddef7cda838f480d43
Jesus Manuel Olivas <jesus.olivas@gmail.com>
2014-05-30 11:55:07 -0700

    Render a Template

--- src/DrupalCon/FirstBundle/Controller/HelloController.php
+++ src/DrupalCon/FirstBundle/Controller/HelloController.php
@@ -2,12 +2,30 @@
 
 namespace DrupalCon\FirstBundle\Controller;
 
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
 
-class HelloController
+class HelloController extends Controller
 {
   public function indexAction()
   {
     return new Response('<html><body><h1>Hello World</h1></body></html>');
+  }
+
+  public function templateAction()
+  {
+    return $this->render('FirstBundle:Hello:template.html.twig');
+  }
+
+  /**
+   * @Template()
+   */
+  public function helloAction($name, $times)
+  {
+    return [
+      'name'=>$name,
+      'times' => $times
+    ];
   }
 }
