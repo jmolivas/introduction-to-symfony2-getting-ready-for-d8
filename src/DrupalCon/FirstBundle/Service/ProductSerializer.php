ac4f6e9cadc858e48f1a0f35c913cab76defd97b
Ryan Weaver <ryan@thatsquality.com>
2014-06-01 13:33:47 -0500

    Injecting the router into the serializer

--- src/DrupalCon/FirstBundle/Service/ProductSerializer.php
+++ src/DrupalCon/FirstBundle/Service/ProductSerializer.php
@@ -3,15 +3,24 @@
 namespace DrupalCon\FirstBundle\Service;
 
 use DrupalCon\FirstBundle\Entity\Product;
+use Symfony\Component\Routing\Router;
 
 class ProductSerializer
 {
+    private $router;
+
+    public function __construct(Router $router)
+    {
+        $this->router = $router;
+    }
+
     public function serialize(Product $product)
     {
         $data = array(
             'id' => $product->getId(),
             'name' => $product->getName(),
             'price' => $product->getPrice(),
+            'url' => $this->router->generate('product_show', array('id' => $product->getId()))
         );
 
         return json_encode($data);
